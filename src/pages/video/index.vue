6c786844812073d3a6102bbc5df6b68736419902
tongjinle <1328192478@qq.com>
2019-01-21 17:35:33 +0800

    左、中、右间距一样，列表播放页一样

--- src/pages/video/index.vue
+++ src/pages/video/index.vue
@@ -1,27 +1,42 @@
 <template>
   <div class="container">
-    <video id="myVideo" :src="videoSrc" :poster="coverSrc" controls objectFit="cover" class="video-container" v-if="!showModal"></video>
+    <video
+      id="myVideo"
+      :src="videoSrc"
+      :poster="coverSrc"
+      controls
+      objectFit="cover"
+      class="video-container"
+      v-if="!showModal"
+    ></video>
     <div class="video-list">
       <div class="video-list-item" v-for="(item,index) in videoList" :key="index">
         <div class="video-list-item-preview">
           <div class="video-list-item-preview-base">
-            <img :src="prefix+item.cover_img" v-if=item.cover_img>  
-          </div>  
+            <img :src="prefix+item.cover_img" v-if="item.cover_img">
+          </div>
           <div class="video-list-item-preview-cover" @click="changeVideo(item,index)">
-            <img src="https://gw.alicdn.com/tfs/TB1D9.IyAvoK1RjSZFNXXcxMVXa-290-200.png" v-if="currentIndex == index">
-            <img src="https://gw.alicdn.com/tfs/TB1ePUKypzqK1RjSZFCXXbbxVXa-290-200.png" v-else>  
-          </div>  
-          <div class="video-list-item-preview-time" v-if="item.time">{{item.time}}</div>  
+            <img
+              src="https://gw.alicdn.com/tfs/TB1D9.IyAvoK1RjSZFNXXcxMVXa-290-200.png"
+              v-if="currentIndex == index"
+            >
+            <img src="https://gw.alicdn.com/tfs/TB1ePUKypzqK1RjSZFCXXbbxVXa-290-200.png" v-else>
+          </div>
+          <div class="video-list-item-preview-time" v-if="item.time">{{item.time}}</div>
         </div>
-        <div class="video-list-item-name">
-          {{item.title}}
-        </div>
+        <div class="video-list-item-name">{{item.title}}</div>
       </div>
     </div>
     <div class="modal" v-if="showModal">
-      <div class="modal-top"><img src="../../assets/video-hint-1.png" alt=""></div>
-      <div class="modal-mid"><img src="../../assets/video-hint-2.png" alt=""></div>
-      <div class="modal-btm" @click="closeModal"><img src="../../assets/video-hint-3.png" alt=""></div>
+      <div class="modal-top">
+        <img src="../../assets/video-hint-1.png" alt>
+      </div>
+      <div class="modal-mid">
+        <img src="../../assets/video-hint-2.png" alt>
+      </div>
+      <div class="modal-btm" @click="closeModal">
+        <img src="../../assets/video-hint-3.png" alt>
+      </div>
     </div>
   </div>
 </template>
@@ -38,11 +53,11 @@ export default {
       showModal: false,
       currentIndex: 0,
       videoId: "",
-      videoList:[],
+      videoList: [],
       coverSrc:
         "https://gw.alicdn.com/tfs/TB1oAI2yxnaK1RjSZFtXXbC2VXa-1136-640.png",
-      videoSrc:""
-        // "http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400"
+      videoSrc: ""
+      // "http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400"
     };
   },
 
@@ -53,14 +68,14 @@ export default {
       wx.setStorageSync("firstvideo", true);
       this.showModal = false;
     },
-    changeVideo(item,index) {
+    changeVideo(item, index) {
       this.videoCtx.stop();
       this.coverSrc = this.prefix + item.cover_img;
       this.videoSrc = item.video_url;
       this.currentIndex = index;
       wx.setNavigationBarTitle({
         title: item.title
-      })
+      });
     }
   },
 
@@ -74,27 +89,27 @@ export default {
     this.videoId = option.id;
     this.videoSrc = option.video_url;
     wx.request({
-      url: config.base + 'Attraction/VideoList', //开发者服务器接口地址",
-      data: 'data', //请求的参数",
-      method: 'GET',
-      dataType: 'json', //如果设为json，会尝试对返回的数据做一次 JSON.parse
+      url: config.base + "Attraction/VideoList", //开发者服务器接口地址",
+      data: "data", //请求的参数",
+      method: "GET",
+      dataType: "json", //如果设为json，会尝试对返回的数据做一次 JSON.parse
       success: res => {
         const data = res.data && res.data.data;
         this.videoList = data;
-        if(this.videoList.length>0) {
-          const currentItem = this.videoList.filter((item,index) => {
-            if (item.id == option.id ) {
+        if (this.videoList.length > 0) {
+          const currentItem = this.videoList.filter((item, index) => {
+            if (item.id == option.id) {
               this.currentIndex = index;
-              return true
+              return true;
             } else {
-              return false
+              return false;
             }
           })[0];
           this.coverSrc = this.prefix + currentItem.cover_img;
           this.videoSrc = currentItem.video_url;
           wx.setNavigationBarTitle({
             title: currentItem.title
-          })
+          });
         }
       },
       fail: () => {},
@@ -121,11 +136,11 @@ export default {
 .video-list {
   height: 62%;
   overflow: scroll;
-  padding: 0 20rpx;
+  padding: 0 10rpx;
   background: #fff;
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-between;
+  justify-content: space-around;
   &-item {
     margin: 20rpx 0;
     &-preview {
@@ -134,15 +149,20 @@ export default {
       border-radius: 10rpx;
       overflow: hidden;
       position: relative;
-      &-base, &-cover {
+      &-base,
+      &-cover {
         position: absolute;
-        top: 0;left: 0;right: 0;bottom: 0;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
         width: 100%;
         height: 100%;
       }
       &-time {
         position: absolute;
-        right: 0;bottom: 0;
+        right: 0;
+        bottom: 0;
         width: 82rpx;
         height: 48rpx;
         line-height: 48rpx;
@@ -150,10 +170,10 @@ export default {
         border-top-left-radius: 20rpx;
         font-size: 20rpx;
         text-align: center;
-        color:#fff;
+        color: #fff;
       }
     }
-    &-name{
+    &-name {
       font-size: 26rpx;
       width: 340rpx;
       overflow: hidden;
@@ -165,7 +185,7 @@ export default {
   bottom: 10rpx;
   right: 0;
   z-index: 99;
-  .img{
+  .img {
     width: 112rpx;
     height: 100rpx;
   }
@@ -176,7 +196,7 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background: rgba(0,0,0,.6);
+  background: rgba(0, 0, 0, 0.6);
   z-index: 999;
   &-top {
     position: absolute;
