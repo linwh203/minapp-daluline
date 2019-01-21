c0222a4d8c2cc4aa020c86f2b27e4bdef1fa1991
tongjinle <1328192478@qq.com>
2019-01-21 18:16:55 +0800

    17.观察笔记换成绿色

--- src/pages/my-note/index.vue
+++ src/pages/my-note/index.vue
@@ -2,10 +2,14 @@
   <div class="container">
     <div class="note-area">
       <div class="textarea">
-        <textarea 
+        <textarea
           :placeholder="holderText"
           :placeholder-style="style"
-          @focus="onFocus" auto-height fixed v-model="text"/>
+          @focus="onFocus"
+          auto-height
+          fixed
+          v-model="text"
+        />
       </div>
       <div class="submit" @click="submit">发布</div>
       <cover-view class="note-area-icon" v-if="showHolder">
@@ -13,17 +17,18 @@
       </cover-view>
     </div>
     <div class="main">
-      <div class="main-item" v-for="(item,index) in noteList" :key=item.id>
+      <div class="main-item" v-for="(item,index) in noteList" :key="item.id">
         <div class="main-item-left">
-          <img class="main-item-left-icon" src="https://gw.alicdn.com/tfs/TB13VGlxkPoK1RjSZKbXXX1IXXa-35-18.png">
+          <img
+            class="main-item-left-icon"
+            src="https://dl-line.oss-cn-shenzhen.aliyuncs.com/note/ear.png"
+          >
           <div class="main-item-left-number">{{index+1}}</div>
           <div class="main-item-left-line" v-if="index!=noteList.length-1"></div>
         </div>
         <div class="main-item-right">
           <div class="main-item-right-container">
-            <div class="main-item-right-up">
-              {{item.content}}
-            </div>
+            <div class="main-item-right-up">{{item.content}}</div>
             <div class="main-item-right-down">
               <div class="main-item-right-down-time">{{item.time}}</div>
               <div class="main-item-right-down-del" @click="openDialog(item.id)">删除</div>
@@ -57,10 +62,10 @@ export default {
       noteList: [],
       userCode: "",
       isFoucs: false,
-      currentId:"",
-      dialog:false,
-      showHolder:true,
-      holderText:"__ 留下笔记"
+      currentId: "",
+      dialog: false,
+      showHolder: true,
+      holderText: "__ 留下笔记"
     };
   },
 
@@ -82,16 +87,18 @@ export default {
     },
     confirmDel() {
       const token = wx.getStorageSync("userCode");
-      const delUrl = `${config.base}Note/deleteNoteList?token=${token}&id=${this.currentId}`;
+      const delUrl = `${config.base}Note/deleteNoteList?token=${token}&id=${
+        this.currentId
+      }`;
       wx.request({
         // url: config.base + 'Note/deleteNoteList', //开发者服务器接口地址"
         url: delUrl, //开发者服务器接口地址"
-        method: 'DELETE',
+        method: "DELETE",
         // data: {
         //   token: token,
         //   id: this.currentId
         // },
-        dataType: 'json', //如果设为json，会尝试对返回的数据做一次 JSON.parse
+        dataType: "json", //如果设为json，会尝试对返回的数据做一次 JSON.parse
         success: res => {
           wx.showToast({
             title: "删除成功", //提示的内容,
@@ -157,21 +164,21 @@ export default {
     getNoteList() {
       const token = wx.getStorageSync("userCode");
       wx.request({
-        url: config.base + 'note/noteList', //开发者服务器接口地址",
+        url: config.base + "note/noteList", //开发者服务器接口地址",
         // data: {
         //   token:token
         // }, //请求的参数",
         header: {
           token: token
         },
-        method: 'GET',
-        dataType: 'json', //如果设为json，会尝试对返回的数据做一次 JSON.parse
+        method: "GET",
+        dataType: "json", //如果设为json，会尝试对返回的数据做一次 JSON.parse
         success: res => {
-          console.log('note',res.data)
+          console.log("note", res.data);
           this.noteList = res.data && res.data.data;
           this.noteList.forEach(item => {
             item.time = formatDate(item.time);
-          })
+          });
         },
         fail: () => {},
         complete: () => {}
@@ -191,92 +198,93 @@ export default {
 .container {
   background: #f3f3f3;
 }
-.main{
+.main {
   padding: 48rpx 20rpx 90rpx;
   background: #f3f3f3;
-  &-item{
+  &-item {
     display: flex;
-    &-left{
+    &-left {
       margin-right: 20rpx;
       display: flex;
       flex-direction: column;
       align-items: center;
       position: relative;
-      &-number{
+      &-number {
         width: 66rpx;
         line-height: 66rpx;
         text-align: center;
-        background: #292770;
-        color:#fff;
+        background: #2b9f62;
+        color: #fff;
         border-radius: 50%;
       }
-      &-line{
+      &-line {
         height: 100%;
         width: 2px;
-        background: #292770;
+        background: #2b9f62;
       }
-      &-icon{
+      &-icon {
         width: 36rpx;
         height: 18rpx;
         position: absolute;
         top: -14rpx;
         left: 0;
         right: 0;
-        margin:auto;
+        margin: auto;
       }
     }
-    &-right{
+    &-right {
       width: 100%;
       font-size: 28rpx;
-      &-up{
-        padding:20rpx;
+      &-up {
+        padding: 20rpx;
       }
-      &-container{
+      &-container {
         background: #fff;
-        border:1px solid #eaebea;
-        min-height:240rpx;
+        border: 1px solid #eaebea;
+        min-height: 240rpx;
         border-radius: 8rpx;
         display: flex;
         flex-direction: column;
         justify-content: space-between;
       }
-      &-margin{
+      &-margin {
         height: 48rpx;
       }
-      &-down{
+      &-down {
         height: 60rpx;
-        border-top:1px dashed #dcdcdc;
+        border-top: 1px dashed #dcdcdc;
         display: flex;
         justify-content: space-between;
         align-items: center;
-        &-time{
-          color:#cacaca;
+        &-time {
+          color: #cacaca;
           margin-left: 20rpx;
         }
-        &-del{
+        &-del {
           width: 78rpx;
           line-height: 38rpx;
           text-align: center;
           border-radius: 10rpx;
-          color:#6f6f6f;
-          border:1px dashed #868686;
+          color: #6f6f6f;
+          border: 1px dashed #868686;
           margin-right: 20rpx;
         }
       }
     }
   }
 }
-.note-area{
+.note-area {
   display: flex;
   align-items: center;
-  position:fixed;
+  position: fixed;
   bottom: 0rpx;
-  left: 0rpx;right: 0rpx;
-  padding:20rpx;
-  border-top:1px solid #dcdcdc;
+  left: 0rpx;
+  right: 0rpx;
+  padding: 20rpx;
+  border-top: 1px solid #dcdcdc;
   background: #f3f3f3;
   z-index: 88;
-  &-icon{
+  &-icon {
     position: fixed;
     left: 38rpx;
     bottom: 62rpx;
@@ -285,14 +293,14 @@ export default {
   }
 }
 .textarea {
-  min-height:36rpx;
+  min-height: 36rpx;
   font-size: 30rpx;
   border-radius: 12rpx;
-  border:1px solid #dcdcdc;
+  border: 1px solid #dcdcdc;
   width: 78%;
   background: #fff;
   padding: 24rpx;
-  textarea{
+  textarea {
     width: 100%;
   }
 }
@@ -313,46 +321,53 @@ export default {
   align-items: center;
   position: relative;
 }
-.dialog{
-  position: fixed;z-index: 999;
-  top: 0;bottom: 0;left: 0;right: 0; background: rgba(0,0,0,.5);
-  display: flex;justify-content: center;align-items:center;
-  &-box{
+.dialog {
+  position: fixed;
+  z-index: 999;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &-box {
     width: 380rpx;
     height: 380rpx;
-    padding:20rpx;
+    padding: 20rpx;
     border-radius: 20rpx;
     background: #fff;
     display: flex;
     flex-direction: column;
     align-items: center;
     font-size: 36rpx;
-    &-first{
+    &-first {
       margin-top: 80rpx;
       margin-bottom: 20rpx;
     }
-    &-second{
+    &-second {
       margin-bottom: 96rpx;
     }
-    &-btns{
+    &-btns {
       width: 100%;
       display: flex;
       justify-content: space-between;
       align-items: center;
-      &-item{
+      &-item {
         width: 160rpx;
         line-height: 80rpx;
-        color:#fff;
+        color: #fff;
         text-align: center;
         border-radius: 10rpx;
       }
     }
   }
 }
-.confirm{
-  background: #292770;
+.confirm {
+  background: #2b9f62;
 }
-.cancel{
+.cancel {
   background: #6f6f6f;
 }
 </style>
